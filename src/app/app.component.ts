8545ee23362b607ff1254871437c00edb220db42
Reeve <lauree@esquel.com>
2020-04-09 11:11:02 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,15 @@ export class AppComponent {
   title = 'ngx-videojs';
 
   narrowband = { 
+    autoplay: false,
     sources: [{
       src: 'https://mts-test.esquel.cloud/narrowband/output/8d6009db44d74c88a32ca70a9faf4051/narrow4/eel_pantry_4k-60fps-iphone_x.m3u8',
       type: 'application/x-mpegURL'
     }]
   };
 
-  standard = { 
+  standard = {
+    autoplay: false,
     sources: [{
       src: 'https://mts-test.esquel.cloud/normal/output/b729c4af064a49af988d4278cb0964b4/normal4/eel_pantry_4k-60fps-iphone_x.m3u8',
       type: 'application/x-mpegURL'
