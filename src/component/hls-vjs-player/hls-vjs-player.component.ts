afc5748d1d6c6c0da537f647067ee084c3bd4a64
Reeve <lauree@esquel.com>
2020-04-09 10:43:36 +0800

    update

---
+++ src/component/hls-vjs-player/hls-vjs-player.component.ts
@@ -0,0 +1,55 @@
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import videojs from 'video.js';
+
+@Component({
+  selector: 'hls-vjs-player',
+  template: `
+    <video #target class="video-js"  >
+    </video>
+  `,
+  styleUrls: [
+    './hls-vjs-player.component.css'
+  ],
+  encapsulation: ViewEncapsulation.None,
+})
+export class HlsVjsPlayerComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('target') target: ElementRef;
+  // see options: https://github.com/videojs/video.js/blob/mastertutorial-options.html
+  @Input() options: videojs.PlayerOptions;
+  
+  
+  player: videojs.Player;
+
+  constructor(
+    private elementRef: ElementRef,
+  ) { }
+
+  ngOnInit() {
+    // instantiate Video.js
+  }
+
+  ngAfterViewInit() {
+    let defaultOptions : videojs.PlayerOptions = {
+      controls: true,
+      autoplay: true,
+      preload: 'auto',
+      techOrder: ['html5'],
+      fluid: true
+    };
+
+    let inputOptions :  videojs.PlayerOptions = Object.assign(defaultOptions, this.options);
+
+    this.player = videojs(this.target.nativeElement, inputOptions, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    });
+
+    
+  }
+
+  ngOnDestroy() {
+    // destroy player
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+}
